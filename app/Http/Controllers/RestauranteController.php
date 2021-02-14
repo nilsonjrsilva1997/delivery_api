580c780d3864412a5992f2bbd5843b45af75c81e
Nilson Junior <nilsonjrsilva97@gmail.com>
2021-02-13 21:55:33 -0300

    validando slug do restaurante

--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Restaurante;
 use Illuminate\Http\Request;
 use App\Unidade;
 
 class RestauranteController extends BaseController
 {
-   
+   public function create(Request $request) 
+   {
+        $validatedData = $request->validate([
+            'slug' => 'required|string|max:255',
+            'nome' => 'required|string|max:255',
+        ]);
+
+        $restauranteSlugCount = Restaurante::where(['slug' => $request->slug])->count();
+
+        if($restauranteSlugCount != 0) {
+            return response(['message' => 'Esse slug já está sendo utilizado por outro restaurante']);
+        }
+
+        return Restaurante::create($validatedData);
+   }
 }
