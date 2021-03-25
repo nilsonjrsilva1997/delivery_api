871083a548c50787617498dfc1345fca27626283
jhonnatthan <jhonnatthan@jhonnatthan.local>
2021-03-25 19:55:50 -0300

    :art: fix meus restaurantes function

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -15,8 +15,19 @@ class UsuarioController extends Controller
         $user_id = Auth::id();
         $restaurantes = Restaurante::query()
             ->where('user_id', $user_id)
-            ->with('unidade')
-            ->get();
+            ->with(['unidade' => function ($q) {
+                $q->with('enderecos')
+                    ->with('tempo_espera_entrega')
+                    ->with('horario_funcionamento')
+                    ->with('sobre_nos')
+                    ->with('restaurante')
+                    ->with(['produtos' => function ($query) {
+                        $query->with('categoria')
+                            ->with(['adicional' => function ($query) {
+                                $query->with('opcoes');
+                            }]);
+                    }]);
+            }])->get();
 
 
         return $restaurantes;
